6eddd8209dbc47b9804da733621e14d08b5b8521
Miyuki <93956624+Miyuki-10@users.noreply.github.com>
2024-03-30 22:05:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("miyuki_profile").addEventListener("click", miyuki_profile);
+document.getElementById("tomyb_profile").addEventListener("click", tomyb_profile);
+document.getElementById("hessien_profile").addEventListener("click", hessien_profile);
+document.getElementById("jim608_profile").addEventListener("click", jim608_profile);
+
+function miyuki_profile() {
+  window.open("https://osu.ppy.sh/users/12705857");
+}
+
+function tomyb_profile() {
+  window.open("https://osu.ppy.sh/users/8375512");
+}
+
+function hessien_profile() {
+  window.open("https://osu.ppy.sh/users/15102102");
+}
+
+function jim608_profile() {
+  window.open("https://osu.ppy.sh/users/10951826");
+}
